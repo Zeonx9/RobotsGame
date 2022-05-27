9da332b769da6aed0838cd26b7c6daaf94119e6c
Egor <dementev.eg@yandex.ru>
2022-05-27 19:02:48 +0300

    button

--- app_client/intfc_classes.h
+++ app_client/intfc_classes.h
@@ -1,4 +1,18 @@
 #ifndef ROBOTSGAME_INTFC_CLASSES_H
 #define ROBOTSGAME_INTFC_CLASSES_H
+#include <SFML/Graphics.hpp>
+
+class Button {
+    public:
+        Button(const char *text, const sf::Font &font, int textSize, int condition, int buttonWidth, int buttonHeight);
+        void changeCondition(int);
+        void mouseOnButton(int, int);
+        int isClick(int, int);
+        void setPosition(int, int);
+        sf::Text draw();
+    private:
+        int width, height, status, size;
+        sf::Text button;
+};
 
 #endif //ROBOTSGAME_INTFC_CLASSES_H
